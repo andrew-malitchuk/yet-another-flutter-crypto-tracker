0ec94403308dc1aaa52aa80f78f21f893fde6974
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-05-07 14:01:33 +0300

    feat: setup data_network

---
+++ packages/data_network/lib/core/network_param.dart
@@ -0,0 +1,11 @@
+class NetworkParam {
+
+  static const String search = 'search';
+  static const String limit = 'limit';
+  static const String offset = 'offset';
+  static const String slug = 'slug';
+  static const String interval = 'interval';
+  static const String start = 'start';
+  static const String end = 'end';
+
+}
