9129944c2eb3808bbd4bc760f6f9db2b765bf1f3
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-05-27 14:05:52 +0300

    feat: setup domain_repository

---
+++ packages/domain_repository/lib/entity/data_response_entity.dart
@@ -0,0 +1,8 @@
+import 'package:domain_repository/entity/base/base_entity.dart';
+
+class DataResponseEntity<T> extends BaseEntity {
+  final int timestamp;
+  final T data;
+
+  DataResponseEntity({required this.timestamp, required this.data});
+}
