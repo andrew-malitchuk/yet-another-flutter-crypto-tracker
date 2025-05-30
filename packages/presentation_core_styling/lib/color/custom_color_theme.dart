b3f6bee8b592d8c4f78a00b085fcaacaf253c851
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-05-30 14:07:30 +0300

    feat: setup presentation_core_styling

---
+++ packages/presentation_core_styling/lib/color/custom_color_theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+extension CustomColors on ColorScheme {
+  Color get neutralN0 => const Color(0xFFFFFFFF);
+  Color get neutralN100 => const Color(0xFFF8F9FA);
+  Color get neutralN200 => const Color(0xFFE9ECEF);
+  Color get neutralN300 => const Color(0xFFDEE2E6);
+  Color get neutralN600 => const Color(0xFF838D95);
+  Color get neutralN900 => const Color(0xFF212529);
+  Color get primaryN200 => const Color(0xFFE5E9FD);
+  Color get primaryN600 => const Color(0xFF6C7AA2);
+  Color get primaryN900 => const Color(0xFF5772F2);
+  Color get secondaryN100 => const Color(0xFF206F66);
+  Color get secondaryN900 => const Color(0xFF5DC5BC);
+  Color get secondaryN1000 => const Color(0xFF206F66);
+  Color get successN900 => const Color(0xFF48AD61);
+  Color get errorN200 => const Color(0xFFF9DEDD);
+  Color get errorN900 => const Color(0xFFBC2C24);
+}
