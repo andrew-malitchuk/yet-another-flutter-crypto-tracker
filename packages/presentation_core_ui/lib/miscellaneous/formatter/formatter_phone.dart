b6b59b478636e93061dd2907897b147192628561
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-06-06 14:16:51 +0300

    feat: setup presentation_core_ui

---
+++ packages/presentation_core_ui/lib/miscellaneous/formatter/formatter_phone.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/services.dart';
+
+class PhoneFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    final text = newValue.text.replaceAll(RegExp(r'\D'), ''); // Remove non-digits
+    final buffer = StringBuffer();
+
+    for (int i = 0; i < text.length; i++) {
+      if (i == 0) buffer.write('(');
+      if (i == 2) buffer.write(') ');
+      if (i == 5 || i == 7) buffer.write(' ');
+      buffer.write(text[i]);
+    }
+
+    return TextEditingValue(
+      text: buffer.toString(),
+      selection: TextSelection.collapsed(offset: buffer.length),
+    );
+  }
+}
