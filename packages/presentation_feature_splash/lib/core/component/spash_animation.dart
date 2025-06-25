e65de44af7c687056eac283b9748aa83fe3474ee
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-06-25 14:25:51 +0300

    feat: setup presentation_feature_splash

---
+++ packages/presentation_feature_splash/lib/core/component/spash_animation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SpashAnimation extends StatelessWidget{
+  const SpashAnimation({super.key});
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
