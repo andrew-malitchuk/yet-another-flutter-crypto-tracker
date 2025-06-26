ebf23be2442f9f7184ff395669ffa79e8a4d6ed7
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-06-26 14:27:42 +0300

    feat: setup presentation_feature_welcome

---
+++ packages/presentation_feature_welcome/lib/bloc/welcome_cubit.dart
@@ -0,0 +1,35 @@
+import 'package:domain_repository/entity/user_profile_entity.dart';
+import 'package:domain_repository/repository/crypto_repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+enum WelcomeStatus { initial, loading, error, goToUserDetails, goToMarket }
+
+class WelcomeState {
+  final WelcomeStatus status;
+
+  const WelcomeState({required this.status});
+
+  // TODO wtf
+  factory WelcomeState.initial() => WelcomeState(status: WelcomeStatus.initial);
+}
+
+
+class WelcomeCubit extends Cubit<WelcomeState> {
+  final CryptoRepository cryptoRepository;
+
+  WelcomeCubit(this.cryptoRepository) : super(WelcomeState.initial());
+
+  Future<void> unauthorize() async {
+
+    await cryptoRepository.saveUserProfile(
+      UserProfileEntity(firstName: "", secondName: "", email: "", phone: "", dateOfBirth: -1)
+    ).then((it){
+      it.fold((onSuccess){
+        emit(WelcomeState(status: WelcomeStatus.goToMarket));
+      }, (onFailure){
+        emit(WelcomeState(status: WelcomeStatus.error));
+      });
+    });
+
+  }
+}
