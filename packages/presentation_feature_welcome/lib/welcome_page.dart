b19ba3e0a4f499213daeb132ab9a4c7157a30e9a
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-06-29 14:28:14 +0300

    feat: setup presentation_feature_welcome

---
+++ packages/presentation_feature_welcome/lib/welcome_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:presentation_feature_welcome/welcome_view.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return WelcomeView();
+  }
+}
