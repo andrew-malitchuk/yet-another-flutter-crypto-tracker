8eb6d8e19bb7b9220403295330fa5e93adb10cba
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-07-04 14:38:05 +0300

    feat: setup presentation_feature_user_profile

---
+++ packages/presentation_feature_user_profile/lib/user_profile_page.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+import 'package:presentation_feature_user_profile/user_profile_view.dart';
+
+class UserProfilePage extends StatelessWidget {
+  const UserProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return UserProfileView();
+  }
+}
