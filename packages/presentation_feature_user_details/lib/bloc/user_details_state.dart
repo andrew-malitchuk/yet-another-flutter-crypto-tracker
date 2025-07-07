145e867ceb32d7956366042024ae11f2f8b71695
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-07-07 14:38:58 +0300

    feat: setup presentation_feature_user_details

---
+++ packages/presentation_feature_user_details/lib/bloc/user_details_state.dart
@@ -0,0 +1,39 @@
+import 'package:equatable/equatable.dart';
+
+sealed class UserDetailsState extends Equatable {}
+
+class UserDetailsInitialState extends UserDetailsState {
+  @override
+  List<Object?> get props => [];
+}
+
+class UserDetailsLoadingState extends UserDetailsState {
+  @override
+  List<Object?> get props => [];
+}
+
+class UserDetailsErrorState extends UserDetailsState {
+  @override
+  List<Object?> get props => [];
+}
+
+class UserDetailsValidationErrorState extends UserDetailsState {
+  String errorMessage;
+
+  UserDetailsValidationErrorState({required this.errorMessage});
+
+  @override
+  List<Object?> get props => [errorMessage];
+}
+
+class UserDetailsLoadedState extends UserDetailsState {
+  UserDetailsLoadedState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class UserDetailsDoneState extends UserDetailsState {
+  @override
+  List<Object?> get props => [];
+}
