81d072ee56d7316431491f9b3866afb52fabc60d
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-07-07 14:38:56 +0300

    feat: setup presentation_feature_user_details

---
+++ packages/presentation_feature_user_details/lib/bloc/user_details_event.dart
@@ -0,0 +1,38 @@
+import 'package:equatable/equatable.dart';
+
+sealed class UserDetailsEvent extends Equatable {}
+
+class UserDetailsLoadEvent extends UserDetailsEvent {
+  @override
+  List<Object?> get props => [];
+}
+
+class UserDetailsSaveEvent extends UserDetailsEvent {
+  final String firstName;
+  final String secondName;
+  final String email;
+  final String operator;
+  final String phone;
+  final int dateOfBirth;
+
+  UserDetailsSaveEvent(this.firstName, this.secondName, this.email,
+      this.operator, this.phone, this.dateOfBirth);
+
+  bool isValid() {
+    return firstName.isNotEmpty &&
+        secondName.isNotEmpty &&
+        email.isNotEmpty &&
+        operator.isNotEmpty &&
+        phone.isNotEmpty &&
+        dateOfBirth > 0;
+  }
+
+  @override
+  List<Object?> get props => [
+        firstName,
+        secondName,
+        email,
+        phone,
+        dateOfBirth,
+      ];
+}
