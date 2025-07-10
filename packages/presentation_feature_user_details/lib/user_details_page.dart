0db3672902356d4d38b515a50edab6032181c78a
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-07-10 14:39:24 +0300

    feat: setup presentation_feature_user_details

---
+++ packages/presentation_feature_user_details/lib/user_details_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:presentation_feature_user_details/user_details_view.dart';
+
+class UserDetailsPage extends StatelessWidget {
+  const UserDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return UserDetailsView();
+  }
+}
