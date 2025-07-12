764c5fc55a02ea40d8e72a0c2bbf88e3c375106f
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-07-12 14:39:50 +0300

    feat: setup presentation_feature_main

---
+++ packages/presentation_feature_main/lib/add_new_coin/add_new_coin_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+import 'add_new_coin_view.dart';
+
+class AddNewCoinPage extends StatelessWidget {
+  const AddNewCoinPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AddNewCoinView();
+  }
+}
