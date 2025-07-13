5d56c6b5b4ffa9f01989f3df86433f82656b3075
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-07-13 14:39:57 +0300

    feat: setup presentation_feature_main

---
+++ packages/presentation_feature_main/lib/add_new_coin/bloc/add_new_coin_bloc.dart
@@ -0,0 +1,89 @@
+import 'package:domain_repository/entity/crypto_asset_entity.dart';
+import 'package:domain_repository/repository/crypto_repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'add_new_coin_event.dart';
+import 'add_new_coin_state.dart';
+
+class AddNewCoinBloc extends Bloc<AddNewCoinEvent, AddNewCoinState> {
+  final CryptoRepository cryptoRepository;
+
+  List<CryptoAssetEntity> data = [];
+  int initLoadOffset = 1;
+  int currentOffset = 1;
+
+  @override
+  AddNewCoinState get initialState => AddNewCoinInitialState();
+
+  AddNewCoinBloc({required this.cryptoRepository})
+      : super(AddNewCoinInitialState()) {
+    on<AddNewCoinLoadEvent>((event, emit) async {
+      emit(AddNewCoinLoadingState());
+
+      // TODO remove this delay, it's just for demo purposes
+      await Future.delayed(Duration(seconds: 2));
+
+      final result = await cryptoRepository.getAssets(
+          null,
+          currentOffset,
+          // TODO: fix
+          10);
+
+      result.fold((success) {
+        data = success.data;
+        if (data.isEmpty) {
+          emit(AddNewCoinEmptyState());
+          return;
+        }
+        emit(AddNewCoinLoadedState(data));
+      }, (failure) {
+        emit(AddNewCoinErrorState());
+      });
+    });
+    on<AddNewCoinLoadMoreEvent>((event, emit) async {
+      currentOffset += 10;
+
+      // TODO remove this delay, it's just for demo purposes
+      await Future.delayed(Duration(seconds: 2));
+
+      final result = await cryptoRepository.getAssets(
+          (state as AddNewCoinLoadedState?)?.query,
+          currentOffset,
+          // TODO: fix
+          10);
+
+      result.fold((success) {
+        data += success.data;
+        emit(AddNewCoinLoadedState(data));
+      }, (failure) {
+        emit(AddNewCoinErrorState());
+      });
+    });
+    on<AddNewCoinSearchEvent>((event, emit) async {
+      emit(AddNewCoinLoadingState());
+
+      // Reset the offset and data for a new search
+      initLoadOffset = 1;
+      currentOffset = 1;
+
+      // Perform the search with the provided query
+      final result = await cryptoRepository.getAssets(
+          event.query,
+          currentOffset,
+          // TODO: fix
+          10);
+
+      result.fold((success) {
+        data = success.data;
+        if (data.isEmpty) {
+          emit(AddNewCoinEmptyState());
+          return;
+        } else {
+          emit(AddNewCoinLoadedState(data)..query = event.query);
+        }
+      }, (failure) {
+        emit(AddNewCoinErrorState());
+      });
+    });
+  }
+}
