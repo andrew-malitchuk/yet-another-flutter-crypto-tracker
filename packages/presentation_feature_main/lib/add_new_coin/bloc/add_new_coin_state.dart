3cb0671df469df56302fea00f779e97641a08024
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-07-13 14:40:03 +0300

    feat: setup presentation_feature_main

---
+++ packages/presentation_feature_main/lib/add_new_coin/bloc/add_new_coin_state.dart
@@ -0,0 +1,35 @@
+import 'package:domain_repository/entity/crypto_asset_entity.dart';
+import 'package:equatable/equatable.dart';
+
+sealed class AddNewCoinState extends Equatable {}
+
+class AddNewCoinInitialState extends AddNewCoinState {
+  @override
+  List<Object?> get props => [];
+}
+
+class AddNewCoinLoadingState extends AddNewCoinState {
+  @override
+  List<Object?> get props => [];
+}
+
+class AddNewCoinErrorState extends AddNewCoinState {
+  @override
+  List<Object?> get props => [];
+}
+
+class AddNewCoinLoadedState extends AddNewCoinState {
+  String? query;
+
+  List<CryptoAssetEntity> data;
+
+  AddNewCoinLoadedState(this.data);
+
+  @override
+  List<Object?> get props => [data, query];
+}
+
+class AddNewCoinEmptyState extends AddNewCoinState {
+  @override
+  List<Object?> get props => [];
+}
