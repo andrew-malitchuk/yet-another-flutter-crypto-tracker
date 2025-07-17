320090de626e84eb832dcdc9cf9fe60d762653ab
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-07-17 14:40:51 +0300

    feat: setup presentation_feature_main

---
+++ packages/presentation_feature_main/lib/market/market_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'market_view.dart';
+
+class MarketPage extends StatelessWidget {
+  const MarketPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MarketView();
+  }
+}
