3916c8c99f3edd5b4ee2991aabb538280d690afd
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-07-17 14:40:56 +0300

    feat: setup presentation_feature_main

---
+++ packages/presentation_feature_main/lib/navigation/home_navigation.dart
@@ -0,0 +1,10 @@
+
+
+// class HomeRoute extends GoRouteData {
+//   const HomeRoute();
+//
+//   @override
+//   Widget build(BuildContext context, GoRouterState state) {
+//     return const HomePage();
+//   }
+// }
