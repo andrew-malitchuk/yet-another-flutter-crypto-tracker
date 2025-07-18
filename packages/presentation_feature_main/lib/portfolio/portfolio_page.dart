df2ec516ca50893158aa6b0b7ae800fb620b4d4f
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-07-18 14:41:00 +0300

    feat: setup presentation_feature_main

---
+++ packages/presentation_feature_main/lib/portfolio/portfolio_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:presentation_feature_main/portfolio/portfolio_view.dart';
+
+class PortfolioPage extends StatelessWidget {
+  const PortfolioPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PortfolioView();
+  }
+}
