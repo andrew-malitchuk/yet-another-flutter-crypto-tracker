e320432e5471ff8424e055e00c614dd26b9d59dd
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-07-24 12:53:50 +0300

    feat: update main.dart to include new providers and support Ukrainian localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
-import 'package:data_network/core/network_configuration.dart';
+import 'package:data_database/di/data_database_provider.dart';
 import 'package:data_network/di/data_network_provider.dart';
 import 'package:data_preference/core/initializer/hive_initilizer.dart';
 import 'package:data_repository/di/data_repository_provider.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:presentation_feature_account_settings/di/presentation_feature_account_settings.dart';
 import 'package:presentation_feature_detalization/di/presentation_feature_detalization.dart';
 import 'package:presentation_feature_main/market/di/presentation_feature_main_market.dart';
 import 'package:presentation_feature_user_details/di/presentation_feature_user_details.dart';
+import 'package:presentation_feature_user_profile/di/presentation_feature_user_profile.dart';
 import 'package:provider/provider.dart';
 import 'package:yafct/presentation/feature/app/yafct_app.dart';
 
@@ -17,28 +19,26 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
 
-  // TODO: rename
-  final providers = await initCoreDI();
-
-  print("API Key: ${NetworkConfiguration.apiKey}");
+  final providers = await preference_inirializer();
 
   final allProviders = [
-    // todo wtf
     ...providers,
+    ...dataDatabaseProviders,
     ...dataNetworkProviders,
     ...dataRepositoryProviders,
-    // ... more if needed
-
     ...presentationFeatureMainMarket,
     ...presentationFeatureDetalization,
-    ...presentationFeatureUserDetails
+    ...presentationFeatureUserDetails,
+    ...presentationFeatureUserProfile,
+    ...presentationFeatureAccountSettings,
   ];
 
   runApp(MultiProvider(
       providers: allProviders,
       child: EasyLocalization(
-          supportedLocales: [Locale('en')],
+          supportedLocales: const [Locale('en'), Locale('uk')],
           path: 'assets/translation',
           fallbackLocale: Locale('en'),
+          saveLocale: true,
           child: YafctApp())));
 }
