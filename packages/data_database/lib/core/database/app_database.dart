5f3a43922fe221bdeb987ac5cc170fd12bc55a9c
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-08-01 12:59:11 +0300

    feat: add crypto asset and offline database tables with mappers

---
+++ packages/data_database/lib/core/database/app_database.dart
@@ -0,0 +1,27 @@
+import 'package:data_database/core/database/table/crypto_asset_database_table.dart';
+import 'package:data_database/core/database/table/crypto_offline_database_table.dart';
+import 'package:drift/drift.dart';
+import 'package:drift_flutter/drift_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+
+part 'app_database.g.dart';
+
+@DriftDatabase(tables: [CryptoAssetDatabaseTable, CryptoOfflineDatabaseTable])
+class AppDatabase extends _$AppDatabase {
+  AppDatabase([QueryExecutor? executor]) : super(executor ?? _openConnection());
+
+  @override
+  int get schemaVersion => 1;
+
+  static QueryExecutor _openConnection() {
+    return driftDatabase(
+      name: 'my_database',
+      native: DriftNativeOptions(
+        // By default, `driftDatabase` from `package:drift_flutter` stores the
+        // database files in `getApplicationDocumentsDirectory()`.
+        databaseDirectory: getApplicationSupportDirectory,
+      ),
+      // If you need web support, see https://drift.simonbinder.eu/platforms/web/
+    );
+  }
+}
---
+++ packages/data_database/lib/core/database/table/crypto_asset_database_table.dart
@@ -0,0 +1,52 @@
+import 'package:drift/drift.dart';
+
+import '../../../model/crypto_asset_database_model.dart';
+import '../app_database.dart';
+
+class CryptoAssetDatabaseTable extends Table {
+  IntColumn get id => integer().autoIncrement()();
+
+  TextColumn get name => text().unique()();
+
+  TextColumn get rank => text().nullable()();
+
+  TextColumn get symbol => text().nullable()();
+
+  RealColumn get supply => real().nullable()();
+
+  RealColumn get maxSupply => real().nullable()();
+
+  RealColumn get marketCapUsd => real().nullable()();
+
+  RealColumn get volumeUsd24Hr => real().nullable()();
+
+  RealColumn get priceUsd => real().nullable()();
+
+  RealColumn get changePercent24Hr => real().nullable()();
+
+  RealColumn get vwap24Hr => real().nullable()();
+
+  TextColumn get explorer => text().nullable()();
+
+  BlobColumn get tokens => blob().nullable()();
+
+  RealColumn get amount => real().nullable()();
+}
+
+extension CryptoAssetMapper on CryptoAssetDatabaseTableData {
+  CryptoAssetDatabaseModel toCryptoAsset() => CryptoAssetDatabaseModel(
+      id: id,
+      rank: rank ?? "",
+      symbol: symbol ?? '',
+      name: name,
+      supply: supply ?? 0.0,
+      maxSupply: maxSupply ?? 0.0,
+      marketCapUsd: marketCapUsd ?? 0.0,
+      volumeUsd24Hr: volumeUsd24Hr ?? 0.0,
+      priceUsd: priceUsd ?? 0.0,
+      changePercent24Hr: changePercent24Hr ?? 0.0,
+      vwap24Hr: vwap24Hr ?? 0.0,
+      explorer: explorer ?? '',
+      tokens: {},
+      amount: amount ?? 1.0);
+}
