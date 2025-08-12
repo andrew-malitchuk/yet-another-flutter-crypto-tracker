e43db8f2e9fd1fcec9f6c310c108022003ba86c3
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-08-12 13:01:49 +0300

    refactor: rename hive_initializer.dart to preference_initializer.dart and update method names

--- packages/data_preference/lib/core/initializer/preference_initializer.dart
+++ packages/data_preference/lib/core/initializer/preference_initializer.dart
@@ -1,5 +1,4 @@
 import 'package:data_preference/model/user_profile_prefernce_model.dart';
-import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
@@ -8,13 +7,13 @@ import 'package:provider/single_child_widget.dart';
 import '../../source/preference_source.dart';
 
 // TODO rename this file to preference_initializer.dart
-Future<List<SingleChildWidget>> initCoreDI() async {
+Future<List<SingleChildWidget>> preferenceInitializer() async {
 
   final dir = await getApplicationDocumentsDirectory();
   Hive.init(dir.path);
   Hive.registerAdapter(UserProfileAdapter());
 
-  final userBox = await Hive.openBox<UserProfile>('user_profile_box');
+  final userBox = await Hive.openBox<UserProfilePreferenceModel>('user_profile_box');
 
   return [
     Provider<HiveInterface>.value(value: Hive),
