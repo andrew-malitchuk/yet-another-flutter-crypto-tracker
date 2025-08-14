aa9aff347a8f26fe70cc4d87ada3266409c52557
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-08-14 13:02:08 +0300

    refactor: update PreferenceSource to use UserProfilePreferenceModel and add updateAvatar method

--- packages/data_preference/lib/source/preference_source.dart
+++ packages/data_preference/lib/source/preference_source.dart
@@ -4,15 +4,15 @@ import 'package:hive/hive.dart';
 class PreferenceSource {
   static const _key = 'user_profile';
 
-  final Box<UserProfile> _box;
+  final Box<UserProfilePreferenceModel> _box;
 
   PreferenceSource(this._box);
 
-  Future<void> saveProfile(UserProfile profile) async {
+  Future<void> saveProfile(UserProfilePreferenceModel profile) async {
     await _box.put(_key, profile);
   }
 
-  UserProfile? getProfile() {
+  UserProfilePreferenceModel? getProfile() {
     return _box.get(_key);
   }
 
@@ -23,12 +23,28 @@ class PreferenceSource {
   Future<void> updateEmail(String newEmail) async {
     final current = _box.get(_key);
     if (current != null) {
-      final updated = UserProfile(
+      final updated = UserProfilePreferenceModel(
         firstName: current.firstName,
         secondName: current.secondName,
         email: newEmail,
         phone: current.phone,
+        avatar: current.avatar,
         dateOfBirth: current.dateOfBirth,
+      );
+      await _box.put(_key, updated);
+    }
+  }
+
+  Future<void> updateAvatar(String avatar) async {
+    final current = _box.get(_key);
+    if (current != null) {
+      var updated = UserProfilePreferenceModel(
+        firstName: current.firstName,
+        secondName: current.secondName,
+        email: current.email,
+        phone: current.phone,
+        dateOfBirth: current.dateOfBirth,
+        avatar: avatar
       );
       await _box.put(_key, updated);
     }
