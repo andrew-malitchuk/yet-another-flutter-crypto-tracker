b46d0f38b064a2b15b77287786645cb62aa39241
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-08-16 13:02:25 +0300

    refactor: update data repository providers to include new database datasources

--- packages/data_repository/lib/di/data_repository_provider.dart
+++ packages/data_repository/lib/di/data_repository_provider.dart
@@ -1,3 +1,5 @@
+import 'package:data_database/source/crypto_asset_database_datasource.dart';
+import 'package:data_database/source/crypto_offline_database_datasource.dart';
 import 'package:data_network/source/api_source.dart';
 import 'package:data_preference/source/preference_source.dart';
 import 'package:data_repository/repository/crypto_repository_impl.dart';
@@ -7,6 +9,10 @@ import 'package:provider/provider.dart';
 List<Provider> dataRepositoryProviders = [
   Provider<CryptoRepository>(
     create: (context) => CryptoRepositoryImpl(
-        context.read<ApiSource>(), context.read<PreferenceSource>()),
+        context.read<ApiSource>(),
+        context.read<PreferenceSource>(),
+        context.read<CryptoAssetDatabaseDatasource>(),
+        context.read<CryptoOfflineDatabaseDatasource>()
+    ),
   ),
 ];
