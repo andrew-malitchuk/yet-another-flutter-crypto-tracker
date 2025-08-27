1eb49c2b77d969ebdf05f84c7dd84174b27e6f05
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-08-27 13:06:59 +0300

    feat: add AccountSettingsPage to integrate account settings view

---
+++ packages/presentation_feature_account_settings/lib/account_settings_page.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+import 'account_settings_view.dart';
+
+class AccountSettingsPage extends StatelessWidget {
+  const AccountSettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AccountSettingsView();
+  }
+}
