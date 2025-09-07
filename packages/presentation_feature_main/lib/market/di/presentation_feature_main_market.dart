9ee50fa7d3a9cfa9061fe0c2d7c78d770ffe3227
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-09-07 13:24:48 +0300

    feat: implement SaveNewCoinSearchEvent and success state for adding new coins

--- packages/presentation_feature_main/lib/market/di/presentation_feature_main_market.dart
+++ packages/presentation_feature_main/lib/market/di/presentation_feature_main_market.dart
@@ -3,6 +3,8 @@ import 'package:presentation_feature_main/add_new_coin/bloc/add_new_coin_bloc.da
 import 'package:presentation_feature_main/market/bloc/market_bloc.dart';
 import 'package:provider/provider.dart';
 
+import '../../portfolio/bloc/portfolio_bloc.dart';
+
 List<Provider> presentationFeatureMainMarket = [
   Provider<MarketBloc>(
     create: (context) => MarketBloc(
@@ -11,6 +13,11 @@ List<Provider> presentationFeatureMainMarket = [
   ),
   Provider<AddNewCoinBloc>(
     create: (context) => AddNewCoinBloc(
+      cryptoRepository: context.read<CryptoRepository>(),
+    ),
+  ),
+  Provider<PortfolioBloc>(
+    create: (context) => PortfolioBloc(
       cryptoRepository: context.read<CryptoRepository>(),
     ),
   ),
