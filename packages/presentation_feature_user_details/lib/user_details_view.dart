c9660baba9cb0e2642af00af80f5c0a2bc8c75d6
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-09-17 13:31:52 +0300

    feat: remove unused imports from presentation_feature_user_details.dart

--- packages/presentation_feature_user_details/lib/user_details_view.dart
+++ packages/presentation_feature_user_details/lib/user_details_view.dart
@@ -1,13 +1,11 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:presentation_core_styling/color/custom_color_theme.dart';
 import 'package:presentation_core_styling/typography/custom_text_theme.dart';
 import 'package:presentation_core_ui/layout/safe_scaffold.dart';
 import 'package:presentation_core_ui/layout/scrollable_column.dart';
 import 'package:presentation_core_ui/miscellaneous/formatter/formatter_phone.dart';
-import 'package:presentation_core_ui/widget/button/icon/action_button.dart';
 import 'package:presentation_core_ui/widget/button/primary/primary_button.dart';
 import 'package:presentation_core_ui/widget/field/datetime_field.dart';
 import 'package:presentation_core_ui/widget/field/simple_field.dart';
@@ -17,8 +15,6 @@ import 'package:presentation_core_ui/widget/footer/simple_footer.dart';
 import 'package:presentation_core_ui/widget/header/header_divider_controller.dart';
 import 'package:presentation_core_ui/widget/header/simple_header.dart';
 import 'package:presentation_feature_main/core/navigation/home_navigation.dart';
-import 'package:presentation_feature_main/home_page.dart';
-import 'package:presentation_feature_welcome/dialog/welcome_dialog.dart';
 
 import 'bloc/user_details_bloc.dart';
 import 'bloc/user_details_event.dart';
@@ -39,185 +35,171 @@ class _UserDetailsViewState extends State<UserDetailsView> {
   final SimpleFooterController simpleFooterController =
       SimpleFooterController();
 
-  String firstName = "";
-  String lastName = "";
-  String email = "";
-  String operator = "";
-  String phone = "";
-  int? dateOfBirth = null;
+  final firstNameController = TextEditingController();
+  final secondNameController = TextEditingController();
+  final emailController = TextEditingController();
+  final phoneController = TextEditingController();
+
+  String operatorCode = "";
+  int? dateOfBirth;
+
+  bool _seededFromBloc = false;
 
   @override
   void initState() {
     super.initState();
-
     context.read<UserDetailsBloc>().add(UserDetailsLoadEvent());
   }
 
   @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final colorScheme = theme.colorScheme;
-    final textTheme = theme.textTheme;
-
-    return SafeScaffold(
-      appBar: SimpleHeader(
-          title: null,
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          headerDividerController: headerDividerController),
-      body: _buildView(context),
-    );
+  void dispose() {
+    firstNameController.dispose();
+    secondNameController.dispose();
+    emailController.dispose();
+    phoneController.dispose();
+    super.dispose();
   }
 
-  Widget _buildView(BuildContext context) {
-    final theme = Theme.of(context);
-    final colorScheme = theme.colorScheme;
-    final textTheme = theme.textTheme;
-
-    return BlocListener<UserDetailsBloc, UserDetailsState>(
+  @override
+  Widget build(BuildContext context) {
+    return SafeScaffold(
+      appBar: SimpleHeader(
+        title: null,
+        onPressed: () => Navigator.of(context).pop(),
+        headerDividerController: headerDividerController,
+      ),
+      body: BlocListener<UserDetailsBloc, UserDetailsState>(
         listener: (context, state) {
-          if (state is UserDetailsValidationErrorState) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                backgroundColor: colorScheme.errorN200,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(4))),
-                showCloseIcon: true,
-                closeIconColor: colorScheme.neutralN900,
-                content: Text(
-                  context.tr(state.errorMessage),
-                  style: textTheme.bodyMedium02
-                      .copyWith(color: colorScheme.errorN900),
-                ),
-                behavior: SnackBarBehavior.floating,
-                margin: EdgeInsets.all(16),
-              ),
-            );
-          }
+          if (state is UserDetailsValidationErrorState) {}
           if (state is UserDetailsDoneState) {
             MarketRoute().go(context);
           }
+          if (state is UserDetailsLoadedState && !_seededFromBloc) {
+            _seededFromBloc = true;
+            final p = state.userProfile;
+            firstNameController.text = p?.firstName ?? "";
+            secondNameController.text = p?.secondName ?? "";
+            emailController.text = p?.email ?? "";
+            phoneController.text = p?.phone.replaceAll("+380", "") ?? "";
+          }
         },
         child: BlocBuilder<UserDetailsBloc, UserDetailsState>(
-          bloc: context.read<UserDetailsBloc>(),
           builder: (context, state) {
             switch (state) {
               case UserDetailsInitialState():
-                return LoadingState();
               case UserDetailsLoadingState():
                 return LoadingState();
-              case UserDetailsErrorState():
-                return ErrorState(onClick: null);
+              case UserDetailsLoadedState():
+                return _buildContent(context);
               default:
-                return _buildContent(context, state);
+                return ErrorState(onClick: null);
             }
           },
-        ));
+        ),
+      ),
+    );
   }
 
-  Widget _buildContent(BuildContext context, data) {
+  Widget _buildContent(BuildContext context) {
     final theme = Theme.of(context);
     final colorScheme = theme.colorScheme;
     final textTheme = theme.textTheme;
-    return Column(mainAxisSize: MainAxisSize.max, children: [
-      Expanded(
-        child: ScrollableColumn(
-          onOverScroll: (isOverScroll) {
-            headerDividerController.setVisibility(isOverScroll);
-            simpleFooterController.setVisibility(!isOverScroll);
-          },
-          children: [
-            Padding(
-                padding: EdgeInsets.only(left: 16, right: 16),
-                child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      tr("userDetailsTitle"),
-                      style: textTheme.titleHighlight01.copyWith(
-                        color: colorScheme.neutralN900,
-                      ),
-                      textAlign: TextAlign.left,
-                    ))),
-            Padding(
-                padding: EdgeInsets.only(top: 8, left: 16, right: 16),
-                child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      tr("userDetailsDetails"),
-                      style: textTheme.body02.copyWith(
-                        color: colorScheme.neutralN900,
-                      ),
-                      textAlign: TextAlign.left,
-                    ))),
-            Padding(
-                padding: EdgeInsets.only(top: 24, left: 16, right: 16),
+
+    return Column(
+      children: [
+        Expanded(
+          child: ScrollableColumn(
+            onOverScroll: (isOverScroll) {
+              headerDividerController.setVisibility(isOverScroll);
+              simpleFooterController.setVisibility(!isOverScroll);
+            },
+            children: [
+              Padding(
+                  padding: EdgeInsets.only(left: 16, right: 16),
+                  child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        tr("userDetailsTitle"),
+                        style: textTheme.titleHighlight01.copyWith(
+                          color: colorScheme.neutralN900,
+                        ),
+                        textAlign: TextAlign.left,
+                      ))),
+              Padding(
+                  padding: EdgeInsets.only(top: 8, left: 16, right: 16),
+                  child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        tr("userDetailsDetails"),
+                        style: textTheme.body02.copyWith(
+                          color: colorScheme.neutralN900,
+                        ),
+                        textAlign: TextAlign.left,
+                      ))),
+              Padding(
+                padding: const EdgeInsets.only(top: 24, left: 16, right: 16),
                 child: SimpleField(
+                  controller: firstNameController,
                   label: tr("generalFirstName"),
                   regexPattern: r'^[a-zA-Zа-яА-ЯёЁіІїЇєЄґҐ\s]+$',
-                  onChanged: (value) {
-                    firstName = value;
-                  },
-                )),
-            Padding(
-                padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
                 child: SimpleField(
+                  controller: secondNameController,
                   label: tr("generalLastName"),
                   regexPattern: r'^[a-zA-Zа-яА-ЯёЁіІїЇєЄґҐ\s]+$',
-                  onChanged: (value) {
-                    lastName = value;
-                  },
-                )),
-            Padding(
-                padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
                 child: SimpleField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
                   label: tr("generalEmail"),
                   regexPattern: r'^[a-zA-Z0-9._%+-@]*$',
-                  onChanged: (value) {
-                    email = value;
-                  },
-                )),
-            Padding(
-                padding: EdgeInsets.only(top: 16, left: 16, right: 16),
-                child: Row(children: [
-                  Flexible(
-                    flex: 1,
-                    child: OperatorField(
-                      operators: [
-                        OperatorValue(
-                          label: "+380",
-                          icon: 'assets/icon/icon-ua-flag-24.svg',
-                        ),
-                      ],
-                      hint: tr("generalOperatorArea"),
-                      onSaved: (value) {
-                        // setState(() {
-                        operator = value ?? "";
-                        // });
-                      },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+                child: Row(
+                  children: [
+                    Flexible(
+                      child: OperatorField(
+                        operators: [
+                          OperatorValue(
+                              label: "+380",
+                              icon: 'assets/icon/icon-ua-flag-24.svg')
+                        ],
+                        hint: tr("generalOperatorArea"),
+                        onSaved: (value) {
+                          setState(() {
+                            operatorCode = value ?? "";
+                          });
+                        },
+                      ),
                     ),
-                  ),
-                  SizedBox(width: 16),
-                  Flexible(
-                    flex: 2,
-                    child: SimpleField(
-                      label: tr("generalPhone"),
-                      inputFormatters: PhoneFormatter(),
-                      length: 14,
-                      onChanged: (value) {
-                        setState(() {
-                          phone = value;
-                        });
-                      },
+                    const SizedBox(width: 16),
+                    Flexible(
+                      flex: 2,
+                      child: SimpleField(
+                        controller: phoneController,
+                        label: tr("generalPhone"),
+                        inputFormatters: PhoneFormatter(),
+                        keyboardType: TextInputType.number,
+                        length: 14,
+                      ),
                     ),
-                  ),
-                ])),
-            Padding(
-                padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
                 child: DateTimeField(
                   label: (dateOfBirth != null)
                       ? DateFormat('dd/MM/yyyy').format(
-                          DateTime.fromMillisecondsSinceEpoch(dateOfBirth ?? 0))
+                          DateTime.fromMillisecondsSinceEpoch(dateOfBirth!))
                       : tr("generalDateOfBirth"),
                   onTap: () {
                     showDatePicker(
@@ -233,24 +215,30 @@ class _UserDetailsViewState extends State<UserDetailsView> {
                       }
                     });
                   },
-                )),
-          ],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      SimpleFooter(
+        SimpleFooter(
           simpleFooterController: simpleFooterController,
           content: PrimaryButton(
             onClick: () {
-              context.read<UserDetailsBloc>().add(UserDetailsSaveEvent(
-                  firstName,
-                  lastName,
-                  email,
-                  operator,
-                  phone,
-                  dateOfBirth ?? 0));
+              context.read<UserDetailsBloc>().add(
+                    UserDetailsSaveEvent(
+                      firstNameController.text,
+                      secondNameController.text,
+                      emailController.text,
+                      operatorCode,
+                      phoneController.text,
+                      dateOfBirth ?? 0,
+                    ),
+                  );
             },
             title: tr("generalContinue"),
-          ))
-    ]);
+          ),
+        ),
+      ],
+    );
   }
 }
