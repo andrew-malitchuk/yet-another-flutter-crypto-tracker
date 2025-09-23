3fc04e5f86a9143cdb940f967018d4fb9c9256ee
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-09-23 13:32:42 +0300

    refactor: remove unused import of crypto_repository in welcome_view

--- packages/presentation_feature_welcome/lib/welcome_view.dart
+++ packages/presentation_feature_welcome/lib/welcome_view.dart
@@ -1,4 +1,3 @@
-import 'package:domain_repository/repository/crypto_repository.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
